e4346e0f3fc6a463b94f57ae1976b3041ad4355f
Marcelo Machado <mjoelmachadojr@gmail.com>
2022-04-30 04:07:29 -0300

    add navigation menu
    nav menu with hide && show function

---
+++ src/assets/js/main.js
@@ -0,0 +1,54 @@
+/*============ show menu ============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+/*==== menu show ====*/
+/* validate if const exists */
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+/*==== menu hidden ====*/
+/* validate if const exists */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==== remove menu mobile ====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // when click on each nav__link we remove the show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+
+
+/* show menu */
+
+/* show menu */
+
+/* show menu */
+
+/* show menu */
+
+/* show menu */
+
+/* show menu */
+
+/* show menu */
+
+/* show menu */
+
+/* show menu */
