0bc865d316d3e65e76fa06e073ed992dce80e31b
Marcelo Machado <mjoelmachadojr@gmail.com>
2022-05-06 19:11:54 -0300

    Webpage fully functional
    Project complete

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -62,25 +62,47 @@ let newSwiper = new Swiper(".new-swiper", {
 
 
 /*============ scroll sections active link ============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*============ show scroll up ============*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*============ scroll reveal animation ============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+sr.reveal('.home-swiper, .new-swiper, .newsletter__container')
+sr.reveal('.category__data, .trick__content, .footer__content', {interval: 100})
+sr.reveal('.about__data, .discount__img', {origin: 'left'})
+sr.reveal('.about__img, .discount__data', {origin: 'right'})
+
 
 /* show menu */
 
-/* show menu */
-
-/* show menu */
-
-/* show menu */
-
-/* show menu */
-
-/* show menu */
-
-/* show menu */
-
-/* show menu */
-
-/* show menu */
